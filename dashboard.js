7287cb28f009610e0169cf5a364305f73b0e5725
jagathda <mail.jagath@gmil.com>
2024-03-12 00:20:21 +0200

    Add JS validation, CSS and changes in HTML

--- dashboard.js
+++ dashboard.js
@@ -1,56 +1,59 @@
 document.getElementById('search-button').addEventListener('click', function() {
     const inputCity = document.getElementById('search-input').value;
+
+    //Validation input city
+    if(!inputCity) {
+        alert('Enter valid city');
+    }
+
     getWeatherData(inputCity);
+    document.getElementById('search-input').value = "";
 });
 
-const API_KEY = '5c11e2726317d6be54e269d0335e5366';
+const getWeatherData = async (cityName) => {
 
-const getWeatherData = async(city) => {
+    const apiKey = '5c11e2726317d6be54e269d0335e5366';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
     try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+        const response = await fetch(url);
         const data = await response.json();
 
         //Logging fetched data
         console.log(data);
-        console.log("City : " + data.name)
 
-        console.log("Temperature : " + data.main.temp);
-        //Converting temperature to Celsius
-        // const tempInCelsius = ((data.main.temp) - 273.15).toFixed(2);
-        const tempInCelsius = Math.round((data.main.temp) - 273.15);
-        console.log("Temperature in Celsius: " + tempInCelsius);
-        //Converting temperature to Fahrenheit
-        const tempInFahrenheit = Math.round((tempInCelsius * 9/5) + 32);
-        console.log("Temperature in Fahrenheit: " + tempInFahrenheit);
-
-        console.log("Humidity % : " + data.main.humidity);
-        console.log("Wind speed : " + data.wind.speed);
-        console.log("Weather condition : " + data.weather[0].description);
-
-        //Display on the webpage
-        document.getElementById('city-name').innerText = `City : ${data.name}`;
-        document.getElementById('temperatureC').innerText = `Temperature : ${tempInCelsius} °C`;
-        document.getElementById('temperatureF').innerText = `Temperature : ${tempInFahrenheit} °F`;
-        document.getElementById('humidity').innerText = `Humidity : ${data.main.humidity} %`;
-        document.getElementById('wind-speed').innerText = `Wind Speed : ${data.wind.speed} m/s`;
-        document.getElementById('Weather-condition').innerText = `Weather Condition : ${data.weather[0].description.toUpperCase()}`;
-        
-        //Set Weather condition icon
+        //Assigning variables
+        const city = data.name;
+        const tempInCelsius = (data.main.temp).toFixed(0);
+        const tempInFahrenheit = ((data.main.temp * (9/5)) + 32).toFixed(0);
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+        const weatherCondition = data.weather[0].description;
         const weatherConditionIcon = data.weather[0].icon;
+
+        //Logging data on variables
+        console.log("City : " + city);
+        console.log("Temperature in C: " + tempInCelsius);
+        console.log("Temperature in F: " + tempInFahrenheit);
+        console.log("Humidity % : " + humidity);
+        console.log("Wind speed in m/s : " + windSpeed);
+        console.log("Weather condition : " + weatherCondition);
         console.log("Weather icon : " + weatherConditionIcon);
 
-        //Set Weather Icon to empty initially
-        document.getElementById('weather-icon').src = '';
-        document.getElementById('weather-icon').alt = '';
-
-        //Set Weather Icon to visible
-        if (weatherConditionIcon){
-            // https://openweathermap.org/img/wn/10d@2x.png
-            const weatherConditionIconUrl = `https://openweathermap.org/img/wn/${weatherConditionIcon}.png`;
-            document.getElementById('weather-icon').src = weatherConditionIconUrl;
-        }
-      
+        //Display on the webpage
+        document.getElementById('city-name').innerText = `City : ${city}`;
+        document.getElementById('temperatureC').innerText = `Temperature : ${tempInCelsius} °C`;
+        document.getElementById('temperatureF').innerText = `Temperature : ${tempInFahrenheit} °F`;
+        document.getElementById('humidity').innerText = `Humidity : ${humidity} %`;
+        document.getElementById('wind-speed').innerText = `Wind Speed : ${windSpeed} m/s`;
+        document.getElementById('Weather-condition').innerText = `Weather Condition : ${weatherCondition.toUpperCase()}`;
+        
+        //Set Weather condition icon
+        const weatherConditionIconUrl = `https://openweathermap.org/img/wn/${weatherConditionIcon}.png`;
+        document.getElementById('weather-icon').src = weatherConditionIconUrl;
+        document.getElementById('weather-icon').alt = `${weatherCondition} image`;
+        
     } catch (error) {
-        console.log(error);
+        console.log('Error', error);
     }
 }
